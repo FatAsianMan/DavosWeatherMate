bdf59ce59d9d463a1b8a48903b385d509b9aaca0
Davo <davodad72@yahoo.com>
2020-02-04 14:04:06 +1000

    Initial commit

---
+++ app/src/main/java/davo/weather/mate/MainActivity.java
@@ -0,0 +1,14 @@
+package davo.weather.mate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
